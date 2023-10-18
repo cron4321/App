37d5452d0ac5ba350f9b74852a5a1ccebfdc48a8
Yeachan1209 <yeow_1@naver.com>
2023-10-18 18:06:18 +0900

    Toggle

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,21 +1,49 @@
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+        setIsSidebarOpen(false);
+      }
+    }
+
+    if (isSidebarOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSidebarOpen]);
+
   return (
     <HeaderContainer>
-      <MenuButton>
+      <MenuButton onClick={toggleSidebar}>
         <MenuIcon sx={{ width: 30, height: 30 }} />
       </MenuButton>
       <AlarmButton>
         <NotificationsIcon sx={{ width: 30, height: 30 }} />
       </AlarmButton>
+      <Sidebar isSidebarOpen={isSidebarOpen} ref={sidebarRef}>
+        <SidebarItem>사용자 or 로그인</SidebarItem>
+        <SidebarItem>학교 변경</SidebarItem>
+      </Sidebar>
     </HeaderContainer>
   );
 }
-
-export default Header;
 
 const HeaderContainer = styled.header`
   display: flex;
@@ -45,3 +73,29 @@ const MenuButton = styled.div`
   align-items: center;
   margin-left: 24px;
 `;
+
+const Sidebar = styled.div<{ isSidebarOpen: boolean }>`
+  width: 250px;
+  height: 100%;
+  background: #333;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  color: #fff;
+  transform: translateX(${(props) => (props.isSidebarOpen ? "0" : "-250px")});
+  transition: transform 0.3s ease-in-out;
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: ${(props) => (props.isSidebarOpen ? 1 : 0)};
+  visibility: ${(props) => (props.isSidebarOpen ? "visible" : "hidden")};
+  overflow: hidden;
+`;
+
+const SidebarItem = styled.div`
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,17 @@ root.render(
   </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js')
+    .then(function (registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function (error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
