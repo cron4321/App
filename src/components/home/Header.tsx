77ae9f5522538324b5519d1707e17fa836b67a31
cron4321 <146915018+cron4321@users.noreply.github.com>
2023-10-31 16:01:19 +0900

    Update Header.tsx
    localhost -> 15.164.241.36

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
     localStorage.removeItem('userEmail');
     localStorage.removeItem('username');
     try {
-      await axios.post("http://localhost:3002/logout");
+      await axios.post("http://15.164.241.36:3002/logout");
       setUserEmail("");
       setUserNickname("");
       setIsLoggedIn(false);
