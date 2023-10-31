639c21b7a333614193ebd29b971179fcaeb9f4e1
cron4321 <146915018+cron4321@users.noreply.github.com>
2023-10-31 16:00:52 +0900

    Update Footer.tsx
    localhost -> 15.164.241.36

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -7,7 +7,7 @@ import EveryChat from "./Chatting/Everychat";
 import ChatIcon from "@mui/icons-material/Chat";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "http://15.164.241.36:3002",
 });
 
 interface Memo {
