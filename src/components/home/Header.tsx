12d8da7480da02bd7bfe2d48b628d3b5a201201f
Yeachan1209 <89767013+Yeachan1209@users.noreply.github.com>
2023-11-01 12:50:18 +0900

    Update Header.tsx

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
     localStorage.removeItem('userEmail');
     localStorage.removeItem('username');
     try {
-      await axios.post("http://43.202.181.150:3002/logout");
+      await axios.post("http://172.31.39.85:3002/logout");
       setUserEmail("");
       setUserNickname("");
       setIsLoggedIn(false);
