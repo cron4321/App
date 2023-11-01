e2e016f6bc3a3f776655d1d7ddcbeff5b8b399fa
Yeachan1209 <89767013+Yeachan1209@users.noreply.github.com>
2023-11-01 12:50:01 +0900

    Update Footer.tsx

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -7,7 +7,7 @@ import EveryChat from "./Chatting/Everychat";
 import ChatIcon from "@mui/icons-material/Chat";
 
 const axios = Axios.create({
-  baseURL: "http://43.202.181.150:3002",
+  baseURL: "http://172.31.39.85:3002",
 });
 
 interface Memo {
