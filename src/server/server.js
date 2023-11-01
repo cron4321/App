2b9e1aaf39ef0adda6f2c3df596d2f827f201f2d
Yeachan1209 <89767013+Yeachan1209@users.noreply.github.com>
2023-11-01 11:33:02 +0900

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -32,7 +32,7 @@ let db;
 
 function connectToDatabase() {
   db = mysql.createConnection({
-    host: '172.31.43.118',
+    host: '10.105.126.107',
     user: '2team',
     password: '1234',
     database: 'projectdb',
@@ -228,7 +228,7 @@ app.post('/logout', (req, res) => {
 });
 
 const connection = mysql.createConnection({
-  host: '172.31.43.118',
+  host: '10.105.126.107',
   user: '2team',
   password: '1234',
   database: 'projectdb',
