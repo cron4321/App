eb0b5f99f1bc8588185bd537fe4edc33c268b7fa
Yeachan1209 <89767013+Yeachan1209@users.noreply.github.com>
2023-11-01 12:26:23 +0900

    Update Main.tsx

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -11,7 +11,7 @@ function Main({ mynumber }: { mynumber: number }) {
 
   useEffect(() => {
     axios
-      .get("http://3.37.37.81:3002/data/")
+      .get("http://43.202.181.150:3002/data/")
       .then((response) => {
         setData(response.data);
       })
