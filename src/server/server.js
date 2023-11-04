e0676a423243377510470c1743854fc1c0f8fbfd
Yeachan1209 <89767013+Yeachan1209@users.noreply.github.com>
2023-11-04 19:49:39 +0900

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -32,11 +32,11 @@ let db;
 
 function connectToDatabase() {
   db = mysql.createConnection({
-    host: '52.78.105.126',
+    host: '10.105.126.107',
     user: '2team',
     password: '1234',
     database: 'projectdb',
-    port: 31212,
+    port: 3306,
   });
 
   db.connect((err) => {
@@ -253,11 +253,11 @@ app.post("/logout", (req, res) => {
 });
 
 const connection = mysql.createConnection({
-  host: '52.78.105.126',
-    user: '2team',
-    password: '1234',
-    database: 'projectdb',
-    port: 31212,
+  host: '10.105.126.107',
+  user: '2team',
+  password: '1234',
+  database: 'projectdb',
+  port: 3306,
 });
 
 connection.connect((err) => {
